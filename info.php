5c978b1e2e48d38e1dd7c22075900c64bf2d1cc0
Marcy Silverman <marcysilve225@gmail.com>
2018-12-14 20:05:02 -0800

    put my name in and was able to use the echo function

--- info.php
+++ info.php
@@ -1,7 +1,16 @@
 <html>
   <body>
     <?php
-    echo "<p> Data processed </>";
-    ?>
+    // echo "<p> Data processed </>";
+    
+    $usersName = Marcy; 
+    $streetAddress = Street; 
+    $cityAddress = Portland; 
+
+    echo $usersName . '<br></br>';
+    echo $streetAddress. '<br></br>';
+    echo $cityAddress. '<br></br>';
+   
+   ?>
   </body>
 </html>
